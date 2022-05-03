9218c309434862d0b6bc2a0d40a82a90e4304ad8
0lucasferrari <lucas.p.f.carvalho@gmail.com>
2022-05-02 22:39:46 -0300

    Adds some comments to the tsx files

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,4 +2,10 @@ function App() {
   return <h1>Hello World!</h1>
 }
 
+// A component is a function that returns HTML. A component always has its first 
+// letter in uppercase.
+
+// To run the project, type in the terminal: npm run dev
+
 export default App
+
--- web/src/main.tsx
+++ web/src/main.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
+// The main file selects the root div in the index.html file and executes the render method.
+// The render method then creates the component appointed below
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
