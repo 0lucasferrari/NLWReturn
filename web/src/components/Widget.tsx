093b83e04a1543c42d9d1897a9b21bfa9720b459
0lucasferrari <lucas.p.f.carvalho@gmail.com>
2022-05-03 00:03:58 -0300

    Adds the headlessui lib to deal with acessibility and changes the Widget component to use it instead of the plain old state management

--- web/src/components/Widget.tsx
+++ web/src/components/Widget.tsx
@@ -1,5 +1,5 @@
+import { Popover } from "@headlessui/react";
 import { ChatTeardropDots } from "phosphor-react";
-import { useState } from "react";
 
 // The outer div positions the button at the bottom right of the screen
 // Overflow-hidden and max-w-0 alongside the pl-2 class makes the Feedback text stay hidden.
@@ -8,25 +8,20 @@ import { useState } from "react";
 
 export function Widget() {
 
-    const [isWidgetOpen, setIsWidgetOpen] = useState(false)
-
-    function toggleWidgetVisibility() {
-        setIsWidgetOpen(!isWidgetOpen)
-    }
 
     return (
-        <div className="absolute bottom-5 right-5">
+        <Popover className="absolute bottom-5 right-5">
 
-            { isWidgetOpen ? <p>Hello World!</p> : null }
+            <Popover.Panel>Hello World!</Popover.Panel>
 
-            <button onClick={toggleWidgetVisibility} className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group">
+            <Popover.Button className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group">
                 <ChatTeardropDots className="w-6 h-6"/>
                 <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear">
                     <span className="pl-2">
                         Feedback
                     </span>
                 </span>
-            </button>
-        </div>
+            </Popover.Button>
+        </Popover>
     )
 }
