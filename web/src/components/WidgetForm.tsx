f1f7e93f296049f72595184de799ff292de394cc
0lucasferrari <lucas.p.f.carvalho@gmail.com>
2022-05-03 19:25:02 -0300

    Makes adjustements to the WidgetForm component

--- web/src/components/WidgetForm.tsx
+++ web/src/components/WidgetForm.tsx
@@ -1,15 +1,13 @@
 export function WidgetForm() {
     return (
-        <div className="bg-zinc-900
-         relative 
-         p-4 rounded-2xl 
-         mb-4 flex-col 
-         items-center 
-         shadow-lg 
-         w-[calc(100vw-2rem)]
-         md:w-auto
-         ">
-            Hello World!
+        <div className="bg-zinc-900 relative p-4 rounded-2xl mb-4 flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
+            <header className="text-xl leading-6">Deixe seu feedback</header>
+
+            <p>Hello World!</p>
+
+            <footer className="text-xs text-neutral-400">
+                Feito com amor pela <a href="http://rocketseat.com.br" className="underline underline-offset-2">RocketSeat</a> 
+            </footer>
         </div>
     )
 }
