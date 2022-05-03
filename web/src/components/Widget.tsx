9c1112d09db38d335cba38d1a7025baf5ff00ea4
0lucasferrari <lucas.p.f.carvalho@gmail.com>
2022-05-03 19:18:05 -0300

    Creates WidgetForm component

--- web/src/components/Widget.tsx
+++ web/src/components/Widget.tsx
@@ -1,5 +1,6 @@
 import { Popover } from "@headlessui/react";
 import { ChatTeardropDots } from "phosphor-react";
+import { WidgetForm } from "./WidgetForm";
 
 // The outer div positions the button at the bottom right of the screen
 // Overflow-hidden and max-w-0 alongside the pl-2 class makes the Feedback text stay hidden.
@@ -10,9 +11,11 @@ export function Widget() {
 
 
     return (
-        <Popover className="absolute bottom-5 right-5">
+        <Popover className="absolute bottom-5 right-5 flex flex-col items-end">
 
-            <Popover.Panel>Hello World!</Popover.Panel>
+            <Popover.Panel>
+                <WidgetForm />
+            </Popover.Panel>
 
             <Popover.Button className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group">
                 <ChatTeardropDots className="w-6 h-6"/>
---
+++ web/src/components/WidgetForm.tsx
@@ -0,0 +1,15 @@
+export function WidgetForm() {
+    return (
+        <div className="bg-zinc-900
+         relative 
+         p-4 rounded-2xl 
+         mb-4 flex-col 
+         items-center 
+         shadow-lg 
+         w-[calc(100vw-2rem)]
+         md:w-auto
+         ">
+            Hello World!
+        </div>
+    )
+}
